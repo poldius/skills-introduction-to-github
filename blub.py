8740f29c0ca70fe061ce87ddba0879056faff552
poldius <leona.gordin@auticon.se>
2026-09-08 08:43:17 +0200

    Add comments with function descriptions

--- blub.py
+++ blub.py
@@ -1,9 +1,12 @@
+#Useless print
 def do_a_thing_idk():
   print("did a thing")
 
+#Factorial of natural number
 def factorial(n):
   if n == 0:
     return 1
   return n*factorial(n-1)
 
+#Do a funny
 do_a_thing()
